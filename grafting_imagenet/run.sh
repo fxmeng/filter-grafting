d313c941f53fcd38290ae51e9cd075c5a104111e
filtergrafting <60565778+filtergrafting@users.noreply.github.com>
2020-02-03 20:32:59 +0800

    Update run.sh

--- grafting_imagenet/run.sh
+++ grafting_imagenet/run.sh
@@ -1,3 +1,2 @@
-CUDA_VISIBLE_DEVICES=0 nohup python3 grafting.py --arch resnet34 --s 1 --num 2 --i 1 >checkpoint/1/1.out &
-CUDA_VISIBLE_DEVICES=1 nohup python3 grafting.py --arch resnet34 --s 1 --num 2 --i 2 >checkpoint/1/2.out &
-#CUDA_VISIBLE_DEVICES=1 nohup python3 baseline.py --arch resnet18 --s 1  >checkpoint/1/nohup.out &
+CUDA_VISIBLE_DEVICES=0 nohup python3 grafting.py --arch resnet18 --s 1 --num 2 --i 1 >checkpoint/1/1.out &
+CUDA_VISIBLE_DEVICES=1 nohup python3 grafting.py --arch resnet18 --s 1 --num 2 --i 2 >checkpoint/1/2.out &
