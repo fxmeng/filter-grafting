729c12d70eb7f6fa79d9da988d9619453ae5106b
filtergrafting <60565778+filtergrafting@users.noreply.github.com>
2020-02-03 23:52:36 +0800

    Update grafting.sh

--- grafting_imagenet/grafting.sh
+++ grafting_imagenet/grafting.sh
@@ -1,2 +1,3 @@
+mkdir -pv checkpoint/grafting_imagenet_resnet18
 CUDA_VISIBLE_DEVICES=0 nohup python grafting.py --arch resnet18 --s grafting_imagenet_resnet18 --num 2 --i 1 >checkpoint/grafting_imagenet_resnet18/1.out &
 CUDA_VISIBLE_DEVICES=1 nohup python grafting.py --arch resnet18 --s grafting_imagenet_resnet18 --num 2 --i 2 >checkpoint/grafting_imagenet_resnet18/2.out &
