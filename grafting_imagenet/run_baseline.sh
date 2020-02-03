5848a7ddf025540137db92cf478d1e6f8fb7fe70
filtergrafting <60565778+filtergrafting@users.noreply.github.com>
2020-02-03 20:34:56 +0800

    Update run_baseline.sh

--- grafting_imagenet/run_baseline.sh
+++ grafting_imagenet/run_baseline.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 nohup python3 baseline.py --arch resnet18 --s 1  >checkpoint/1/nohup.out &
+CUDA_VISIBLE_DEVICES=0 nohup python baseline.py --arch resnet18 --s 1  >checkpoint/1/nohup.out &
