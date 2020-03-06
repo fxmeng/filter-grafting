74fd7a811d16c33f32468fb6471acc1a50db7825
fxmeng <60565778+fxmeng@users.noreply.github.com>
2020-03-06 21:26:00 +0800

    Update grafting.sh

--- grafting_cifar/grafting.sh
+++ grafting_cifar/grafting.sh
@@ -1,3 +1,3 @@
 mkdir -pv checkpoint/grafting_cifar10_resnet32;
-CUDA_VISIBLE_DEVICES=0 nohup python grafting.py --s checkpoint/grafting_cifar10_resnet32 --cifar 10  --model resnet32 --num 2 --i 1  --difflr --cos >checkpoint/grafting_cifar10_resnet32/1.log &
-CUDA_VISIBLE_DEVICES=1 nohup python grafting.py --s checkpoint/grafting_cifar10_resnet32 --cifar 10  --model resnet32 --num 2 --i 2  --difflr --cos >checkpoint/grafting_cifar10_resnet32/2.log &
+CUDA_VISIBLE_DEVICES=0 nohup python grafting.py --s checkpoint/grafting_cifar10_resnet32 --cifar 10  --model resnet32 --num 2 --i 1  >checkpoint/grafting_cifar10_resnet32/1.log &
+CUDA_VISIBLE_DEVICES=1 nohup python grafting.py --s checkpoint/grafting_cifar10_resnet32 --cifar 10  --model resnet32 --num 2 --i 2  >checkpoint/grafting_cifar10_resnet32/2.log &
